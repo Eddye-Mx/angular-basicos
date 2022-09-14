c303ee2a118d3da7da12c68a1085536189fee1f7
Gerardo Linares <gerardo.linares.rojas@gmail.com>
2022-09-14 10:44:53 -0500

    Fin curso bases de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
+import { MainPageComponent } from './dbz/main-page/main-page.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 
@@ -14,7 +16,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     ContadorModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
